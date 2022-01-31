207a5532181002093215ef3b4826207d3e471129
Bern <bernnystrom@gmail.com>
2022-02-01 02:00:21 +0800

    :bulb: (fix): Add more context to each question as comments
    Added the original context of the question from FCC.

--- Convert Celsius to Fahrenheit.playground/Contents.swift
+++ Convert Celsius to Fahrenheit.playground/Contents.swift
@@ -1,5 +1,10 @@
 import UIKit
 
+// The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+
+// You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
+
+
 func convertToF(celsius: Int) -> Int {
     let farenheit = celsius * 9 / 5 + 32
     return farenheit
--- Reverse a String.playground/Contents.swift
+++ Reverse a String.playground/Contents.swift
@@ -1,5 +1,12 @@
 import UIKit
 
+// Reverse the provided string.
+
+// You may need to turn the string into an array before you can reverse it.
+
+// Your result must be a string.
+
+
 func reverseString(str: String) -> String {
     let reversedStr = String(str.reversed())
     return reversedStr
