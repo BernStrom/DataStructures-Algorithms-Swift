778cc6077042dbc2baf5b832a1fd9c865c9d9fa0
Bern <bernnystrom@gmail.com>
2022-02-02 02:25:05 +0800

    🎉 (new): Add algorithm question with completed solution
    Done solution for "Repeat a String Repeat a String" question in Swift with all test cases passed.

--- Repeat a String Repeat a String.playground/Contents.swift
+++ Repeat a String Repeat a String.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+
+
+func repeatStringNumTimes(str: String, num: Int) -> String {
+    var newStr = ""
+    
+    if num > 0 {
+        for _ in 1...num {
+            newStr.append(str)
+        }
+    }
+    
+    return newStr
+}
+
+repeatStringNumTimes(str: "abc", num: 0)
+
+
+// Test Case 1 - Passed ✅
+// repeatStringNumTimes("*", 3) should return the string ***.
+
+// Test Case 2 - Passed ✅
+// repeatStringNumTimes("abc", 3) should return the string abcabcabc.
+
+// Test Case 3 - Passed ✅
+// repeatStringNumTimes("abc", 4) should return the string abcabcabcabc.
+
+// Test Case 4 - Passed ✅
+// repeatStringNumTimes("abc", 1) should return the string abc.
+
+// Test Case 5 - Passed ✅
+// repeatStringNumTimes("*", 8) should return the string ********.
+
+// Test Case 6 - Passed ✅
+// repeatStringNumTimes("abc", -2) should return an empty string ("").
+
+// Test Case 7 - Passed ✅
+// repeatStringNumTimes("abc", 0) should return "".
