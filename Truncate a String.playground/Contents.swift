c009d4772583f4febe7c16330e3bd1c1d2507586
Bern <bernnystrom@gmail.com>
2022-02-02 02:26:20 +0800

    🎉 (new): Add algorithm question with completed solution
    Done "Truncate a String" in Swift with all test cases passed.

--- Truncate a String.playground/Contents.swift
+++ Truncate a String.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+
+func truncateString(str: String, num: Int) -> String {
+    var truncatedStr = ""
+    
+    if str.count > num {
+        truncatedStr = str.replacingCharacters(in: str.index(str.startIndex, offsetBy: num)..<str.endIndex, with: "...")
+    } else {
+        return str
+    }
+    
+    return truncatedStr
+}
+
+truncateString(str: "Peter Piper picked a peck of pickled peppers", num: 11)
+
+
+// Test Case 1
+// truncateString("A-tisket a-tasket A green and yellow basket", 8) should return the string A-tisket... .
+
+// Test Case 2
+// truncateString("Peter Piper picked a peck of pickled peppers", 11) should return the string Peter Piper... .
+
+// Test Case 3
+// truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".count) should return the string A-tisket a-tasket A green and yellow basket.
+
+// Test Case 4
+// truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2) should return the string A-tisket a-tasket A green and yellow basket.
+
+// Test Case 5
+// truncateString("A-", 1) should return the string A... .
+
+// Test Case 6
+// truncateString("Absolutely Longer", 2) should return the string Ab... .
